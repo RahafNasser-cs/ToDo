44eb1b57ab7cf22fd4a3bae2137d9c4cd0fd3022
Rahaf Nasser <rahaf.nasser.ruh.k@tuwaiq.edu.sa>
2021-11-15 10:20:32 +0300

    added basic fun and xml for fragments

--- app/src/main/java/com/example/todo/NewTaskFragment.kt
+++ app/src/main/java/com/example/todo/NewTaskFragment.kt
@@ -5,6 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import com.example.todo.databinding.FragmentNewTaskBinding
+import com.example.todo.model.TaskViewModel
 
 class NewTaskFragment : Fragment() {
+    private var binding: FragmentNewTaskBinding? = null
+    private val sharedViewModel: TaskViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentNewTaskBinding.inflate(inflater, container, false)
+        return binding!!.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding!!.apply {  }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
 }
--- app/src/main/java/com/example/todo/StartFragment.kt
+++ app/src/main/java/com/example/todo/StartFragment.kt
@@ -5,6 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.RecyclerView
+import com.example.todo.adapter.ItemAdapter
+import com.example.todo.data.DataSource
+import com.example.todo.databinding.FragmentStartBinding
+import com.example.todo.model.TaskViewModel
 
 class StartFragment : Fragment() {
+    private var binding: FragmentStartBinding? = null
+    private val sharedViewModel: TaskViewModel by activityViewModels()
+    lateinit var recyclerView: RecyclerView
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentStartBinding.inflate(inflater, container, false)
+        return binding!!.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding!!.apply {}
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
 }
